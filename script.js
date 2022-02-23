45ce1ed0d92bc7ce7f2d479739b9ecba67e7aad5
Justin Lamb <justinlamb@me.com>
2022-02-23 13:39:59 -0800

    added different pen colors and sizes as buttons

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
-const gridcont = document.getElementById('gridcontainer');
-const reset = document.getElementById('reset');
+const GRIDCONT = document.getElementById('gridcontainer');
+const CLEAR = document.getElementById('clear');
+const SMALL = document.getElementById('small');
+const MEDIUM = document.getElementById('medium');
+const LARGE = document.getElementById('large');
+const GREY = document.getElementById('grey');
+const RGB = document.getElementById('rgb');
+const BLACK = document.getElementById('black');
+const ERASER = document.getElementById('eraser');
 let gridItem = document.getElementsByClassName('griditem');
+
+CLEAR.addEventListener('click', () => clearGrid());
+SMALL.addEventListener('click', () => resetGrid(100));
+MEDIUM.addEventListener('click', () => resetGrid(50));
+LARGE.addEventListener('click', () => resetGrid(16));
+GREY.addEventListener('click', () => draw('rgb(128, 128, 128)'));
+BLACK.addEventListener('click', () => draw('rgb(0, 0, 0)'));
+RGB.addEventListener('click', () => draw(randColor(), true));
+ERASER.addEventListener('click', () => draw('rgb(211, 211, 211)'));
+
 createGrid()
-reset.addEventListener('click', () => resetGrid());
 
 function createGrid(num=16) {
-    gridcont.style.gridTemplateColumns = `repeat(${num}, ${900/num}px)`;
-    gridcont.style.gridTemplateRows = `repeat(${num}, ${900/num}px)`;
+    GRIDCONT.style.gridTemplateColumns = `repeat(${num}, ${900/num}px)`;
+    GRIDCONT.style.gridTemplateRows = `repeat(${num}, ${700/num}px)`;
     for (let i = 0; i < num; i++) {
         for (let i = 0; i < num; i++) {
             let div = document.createElement('div');
             div.classList.add('griditem');
-            gridcont.appendChild(div);
+            GRIDCONT.appendChild(div);
         }
     }
     draw();
 }
 
-function draw() {
-
-    Array.from(gridItem).forEach(function(event) {
-        event.addEventListener("mouseenter", function() {
-            this.style.background = "grey";
+function draw(pencolor="rgb(128, 128, 128)", rand=false) {
+    if (rand === true) {
+        Array.from(gridItem).forEach(function(event) {
+            event.addEventListener("mouseenter", function() {
+                this.style.background = randColor();
+            });
         });
-    });
-}
-
-function resetGrid() {
-    // Clear current image.
-    Array.from(gridItem).forEach((item) => item.style.background = '');
-    let num = document.getElementById('num').value;
-    if (num > 100) {
-        prompt("Try again, number cannot exceed 100");
     }
     else {
-        createGrid(num);
-    }
+    Array.from(gridItem).forEach(function(event) {
+        event.addEventListener("mouseenter", function() {
+            this.style.background = pencolor;
+        });
+    });
+}}
+
+function randColor() {
+    let c = Math.floor(Math.random() * 255);
+    let o = Math.floor(Math.random() * 255);
+    let l = Math.floor(Math.random() * 255);
+    return `rgb(${c}, ${o}, ${l})`;
+};
+
+function resetGrid(num) {
+    // Clear current image.
+    Array.from(gridItem).forEach((item) => item.style.background = '');
+    createGrid(num);
 }
 
+function clearGrid() {
+    Array.from(gridItem).forEach((item) => item.style.background = '');
+}
